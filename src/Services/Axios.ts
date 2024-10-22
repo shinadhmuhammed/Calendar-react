62c12ce375ff7cfe5a058b62b37aecc540e95f9f
muhammed shinadh <muhammedshinad2@gmail.com>
2024-10-22 13:12:41 +0530

    removed the with credentials

--- src/Services/Axios.ts
+++ src/Services/Axios.ts
@@ -3,7 +3,6 @@ import axios from 'axios';
 const createAxios = () => {
   const axiosInstance = axios.create({
     baseURL: 'https://calendar-nodejs.onrender.com',  
-    withCredentials:true
   });
 
   axiosInstance.interceptors.request.use(
